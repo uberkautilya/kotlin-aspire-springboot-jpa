c6723774ea8d9a68839daaf69c57498d8aef6965
Mithun <mithuncm@gmail.com>
2023-02-08 14:36:33 +0530

    Start of Predicate list to filter results with JPA

--- src/main/java/io/mcm/springbootwithjava/repository/BookRepository.java
+++ src/main/java/io/mcm/springbootwithjava/repository/BookRepository.java
@@ -1,6 +1,10 @@
 package io.mcm.springbootwithjava.repository;
 
 import io.mcm.springbootwithjava.model.entities.Book;
+import org.apache.catalina.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.query.Param;
@@ -14,4 +18,8 @@ public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificat
             @Param("publishedDate") Date publishedDate,
             @Param("author") String author,
             @Param("isIssues") Boolean isIssued);
+
+    public Page<Book> findAll(Specification<Book> spec, Pageable pageable);
+
+    public List<Book> findAll(Specification<Book> specification);
 }
